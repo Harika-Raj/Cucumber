fea3e950e97048aad0f959984e038b8e9702e8d5
Harika-Raj <r121662@rguktrkv.ac.in>
2018-12-13 11:38:37 +0530

    Add simple scenario and passed it

--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -1,19 +1,24 @@
 module FridayStepHelper
   def is_it_friday(day)
-   'Nope'
-  end
+    if @today == 'Sunday'
+    'Nope'
+    else
+      'Yes'
+      end
+   end
 end
-
 World FridayStepHelper
 
 Given("today is Sunday") do
   @today = 'Sunday'
 end
 
+Given("today is Friday") do
+  @today = 'Friday'
+end
 When("I ask whether it's Friday yet") do
   @actual_answer = is_it_friday(@today)
 end
-
 Then("I should be told {string}") do |expected_answer|
   expect(@actual_answer).to eq(expected_answer)
 end
